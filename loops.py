f8044ac5d7ceb853aee17817d2c8021dc8e7081b
Babul Mirdha <babumirdha@gmail.com>
2025-03-04 00:55:54 +0600

    make understand loop

---
+++ loops.py
@@ -0,0 +1,35 @@
+#two types of loops
+# 1. Definite loop
+# 2. Indefinite loop
+# example of definate loop
+
+print("********* range(10) or range(0,10,1) ************")
+for i in range(0,10,1): # range( i = 0, i < 10, i = i+1 ) or range(0,10,1)
+     print(i)
+
+# example of definate loop with step
+print("********* range(0,10,2) ************")
+for i in range(0,10,2): # range( i = 0, i < 10, i = i+ 2 )
+     print(i)
+
+# example of definate loop with step
+print("********* range(1,10,2) ************")
+for counter in range(1,10,2):
+     print(counter)
+
+# example of definate loop with step by 5
+print("********** range(1,100,5) ***********")
+for counter in range(1,100,5):
+     print(counter)
+     
+# example of definate loop with step by 5
+
+print("******** range(10,100,10) *************")
+for counter in range(10,100,10):
+     print(counter)
+
+
+# while loop
+# for loop
+
+
